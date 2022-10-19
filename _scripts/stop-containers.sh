e2383dfa896cc45492f2c1fed55fc8d41df9f2fa
Matthieu Chan <matthieuchan96@gmail.com>
2022-10-19 12:13:40 +0200

    requests client side

--- _scripts/stop-containers.sh
+++ _scripts/stop-containers.sh
@@ -1,1 +1,1 @@
-docker compose down —-remove-orphans -v
+docker compose down
--- client/main.js
+++ client/main.js
@@ -1,10 +1,55 @@
-const data = document.getElementById('form')
-const title = document.getElementById('title')
-const name = document.getElementById('name')
-const text = document.getElementById('text')
+const data = document.getElementById("form");
+const title = document.getElementById("title");
+const name = document.getElementById("name");
+const text = document.getElementById("text");
 
 for (const el of data.children) {
-    if(el.classList.contains('empty')){
-        el.textContent = el.dataset.placeholder
-    }
+  if (el.classList.contains("empty")) {
+    el.textContent = el.dataset.placeholder;
+  }
 }
+
+// REQUESTS
+
+async function getAll(secrets) {
+  try {
+    const response = await fetch(`http://localhost:3000/${secrets}`);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.warn(err);
+  }
+}
+
+async function getById(secrets, id) {
+  try {
+    const response = await fetch(`http://localhost:3000/${secrets}/${id}`);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.warn(err);
+  }
+}
+
+async function postSecret(e) {
+  e.preventDefault();
+  try {
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(Object.fromEntries(new FormData(e.target))),
+    };
+
+    const response = await fetch("http://localhost:3000/secrets", options);
+    const { id, err } = await response.json();
+    if (err) {
+      throw Error(err);
+    } else {
+      window.location.hash = `#secrets/${id}`;
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+}
+
+//test
